96c1e4790a0f9567b1ce3da59edc1a709b67071c
Conor Owens <conorj.owens@gmail.com>
2019-05-25 22:14:47 +0000

    Automatically backed up by Learn

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,15 @@
 require_relative "./vehicle.rb"
+
+class Car 
+  
+  attr_accessor :wheel_size, :wheel_number
+  
+  def initialize(size, number)
+    @wheel_size = size
+    @wheel_number = number
+  end
+  
+  def go 
+    
+  
+end
